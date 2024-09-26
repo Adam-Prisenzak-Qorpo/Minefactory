dbbb8473e9ee52ef75fed12827325f260b1d67e6
Branislav Hozza <brankohozza@gmail.com>
2024-09-26 19:22:49 +0200

    feat: perlin update

--- Assets/TerrainGeneration.cs
+++ Assets/TerrainGeneration.cs
@@ -1,11 +1,15 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class TerrainGeneration : MonoBehaviour
 {
     public int worldSize = 100;
+    public float heightMultiplier = 2f;
+    public float heightAddition = 10f;
     public Sprite sprite;
-    public float noiseFrequency = 0.05f;
+    public float caveFrequency = 0.05f;
+    public float terrainFrequency = 0.05f;
     public float seed;
     public Texture2D noiseTexture;
 
@@ -25,8 +29,8 @@ public class TerrainGeneration : MonoBehaviour
             for (int y = 0; y < noiseTexture.height; y++)
             {
                 float noiseValue = Mathf.PerlinNoise(
-                    (x + seed) * noiseFrequency,
-                    (y + seed) * noiseFrequency
+                    (x + seed) * caveFrequency,
+                    (y + seed) * caveFrequency
                 );
                 noiseTexture.SetPixel(x, y, new Color(noiseValue, noiseValue, noiseValue));
             }
@@ -39,14 +43,18 @@ public class TerrainGeneration : MonoBehaviour
     {
         for (int x = 0; x < worldSize; x++)
         {
-            for (int y = 0; y < worldSize; y++)
+            var height =
+                Mathf.PerlinNoise((x + seed) * terrainFrequency, seed * terrainFrequency)
+                    * heightMultiplier
+                + heightAddition;
+            for (int y = 0; y < height; y++)
             {
                 float noiseValue = noiseTexture.GetPixel(x, y).r;
-                if (noiseValue > 0.5f)
+                if (noiseValue > 0.2f)
                 {
                     var tile = new GameObject("Tile");
                     tile.transform.parent = this.transform;
-                    tile.transform.position = new Vector2(x, y);
+                    tile.transform.position = new Vector2(x + .5f, y + .5f);
                     tile.AddComponent<SpriteRenderer>().sprite = sprite;
                 }
             }
